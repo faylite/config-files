99cb591968bab0b69eb2fac1f043a787a5f141a8
Faylite <benvev95@gmail.com>
2016-02-15 00:46:40 +0100

    Separated left status into it's own file

---
+++ tmux/scripts/tmux-status-left.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Author: Faylite
+# Custom tmux line, left side
+# Made to be modular and easy to change
+
+# Separators REQUIRES POWERLINE PATCHED FONTS!
+sepL=""
+sepR=""
+
+# Build the status line
+ret=""
+
+# Show current user
+ret+="#[bg=blue]#[fg=white]"
+ret+=" $USER"
+# and current machine/server
+ret+="@$HOSTNAME "
+
+# set -g status-left '#[bg=blue]#[fg=white] #(echo $USER)@#H #[bg=colour236]#[fg=blue] #[fg=white]#S #[bg=colour236]#[bg=colour235]#[fg=colour236]'
+
+# Output new status line
+printf "$ret"
