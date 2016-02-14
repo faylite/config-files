552934e45d249030a7cccf39c380da02b3ccc65e
Faylite <benvev95@gmail.com>
2016-02-14 02:02:31 +0100

    Added a copy of the zsh config files instead of using symlinks

---
+++ .zsh-aliases.zsh
@@ -0,0 +1,18 @@
+# Tmux
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
+
+# Git
+alias gadd='git add'
+alias gcommit='git commit -m'
+alias gpush='git push origin'
+
+# Edit config file shortcuts
+alias ev='vim ~/.vimrc'
+alias et='vim ~/.tmux.conf'
+alias ez='vim ~/.zshrc'
+
+# General
+alias c='clear'
--- zshrc
+++
@@ -1,1 +0,0 @@
-/home/benjamin/.zshrc
