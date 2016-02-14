1b6b5cc5f6db38d58ff65a728e43f1f7cb5a208e
Faylite <benvev95@gmail.com>
2016-02-14 20:32:43 +0100

    Separated right tmux status line into it's own script

--- tmux/scripts/deadbeef_now_playing.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Author: Faylite
-# Prints the currently playing track
-
-deadbeef --nowplaying-tf "%title% - %artist%"
---
+++ tmux/scripts/tmux-status-right.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Author: Faylite
+# Custom tmux line, left side
+# Made to be modular and easy to change
+
+# Separators REQUIRES POWERLINE PATCHED FONTS!
+sepL=""
+sepR=""
+# Get currently playing track info from deadbeef
+current_title=$(deadbeef --nowplaying-tf "%title%")
+current_artist=$(deadbeef --nowplaying-tf "%title%")
+# Get current date in logial format ;)
+current_date=$(date +%d-%m-%Y)
+current_time=$(date +%H:%M)
+
+# Build the status line
+ret=""
+
+# --- Currently Playing deadbeef track ---
+ret+="#[fg=colour236]$sepR#[fg=white]#[bg=colour236]"
+ret+=" ♫ $current_title ♫ "
+
+# --- Current Date
+ret+="#[fg=colour237]$sepR#[fg=white]#[bg=colour237]"
+ret+=" $current_date "
+
+# --- Current Time
+ret+="#[fg=blue]$sepR#[bg=blue]#[fg=white]"
+ret+=" ⌚ $current_time "
+
+# Output new status line
+printf "$ret"
