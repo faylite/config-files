6dc941f6e809b4b5b56b55763fcdc1a9187e1926
Faylite <benvev95@gmail.com>
2016-02-15 01:18:38 +0100

    Added the rest of the original left status line to the script

--- tmux/scripts/tmux-status-left.sh
+++ tmux/scripts/tmux-status-left.sh
@@ -17,6 +17,14 @@ ret+=" $USER"
 # and current machine/server
 ret+="@$HOSTNAME "
 
+# End bit
+ret+="#[bg=colour236]#[fg=blue]$sepL#[fg=white]"
+ret+="#S"
+
+# End bit
+ret+="#[bg=colour236]#[bg=colour235]#[fg=colour236]"
+
+# Original
 # set -g status-left '#[bg=blue]#[fg=white] #(echo $USER)@#H #[bg=colour236]#[fg=blue] #[fg=white]#S #[bg=colour236]#[bg=colour235]#[fg=colour236]'
 
 # Output new status line
