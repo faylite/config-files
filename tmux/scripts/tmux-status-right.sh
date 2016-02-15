21c95b37e0acb05739d6c6bc79c06f8d47efc494
Faylite <benvev95@gmail.com>
2016-02-15 07:41:59 +0100

    Added day name to status, changed some colors and formatting

--- tmux/scripts/tmux-status-right.sh
+++ tmux/scripts/tmux-status-right.sh
@@ -6,31 +6,35 @@
 
 # Separators REQUIRES POWERLINE PATCHED FONTS!
 sepL=""
+sepL2=""
 sepR=""
+sepR2=""
+
 # Get currently playing track info from deadbeef
 current_title=$(deadbeef --nowplaying-tf "%title%")
 current_artist=$(deadbeef --nowplaying-tf "%title%")
 # Get current date in logial format ;)
 current_date=$(date +%d-%m-%Y)
 current_time=$(date +%H:%M)
+current_day=$(date +%a)
 
 # Build the status line
 ret=""
 
-# --- Working Directory ---
-ret+="#[fg=colour236]$sepR#[fg=white]#[bg=colour236]"
-ret+=" $PWD "
-
 # --- Currently Playing deadbeef track ---
-ret+="#[fg=colour208]$sepR#[fg=white]#[bg=colour208]"
+ret+="#[fg=colour237]$sepR#[fg=white]#[bg=colour237]"
+ret+="#[fg=colour202]$sepR#[fg=white]#[bg=colour202]"
 ret+=" ♪ $current_title "
 
 # --- Current Date
-ret+="#[fg=colour237]$sepR#[fg=white]#[bg=colour237]"
-ret+=" $current_date "
+ret+="#[fg=colour238]$sepR#[fg=white]#[bg=colour238]"
+ret+="#[fg=blue]$sepR#[bg=blue]#[fg=white]"
+ret+=" $current_day "
+ret+="#[fg=colour235]$sepR#[bg=colour235]#[fg=white]"
+ret+=" $current_date"
 
 # --- Current Time
-ret+="#[fg=blue]$sepR#[bg=blue]#[fg=white]"
+ret+="#[fg=colour235]$sepR#[bg=colour235]#[fg=blue]$sepR#[bg=blue]#[fg=white]"
 ret+=" ⌚ $current_time "
 
 # Output new status line
