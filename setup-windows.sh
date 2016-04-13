43e7f23d24a570eda9dcffac2f360d230973e7a3
Faylite <benvev95@gmail.com>
2016-04-13 10:14:03 +0200

    Added a setup script for git bash on windows

---
+++ setup-windows.sh
@@ -0,0 +1,13 @@
+# Setup script
+# Author:faylite
+# Sets up a windows machine to my preferred config
+
+# Create the local_env script if it doesn't exist, sourced by zsh for env variables that shouldn't be in version control
+touch ~/.local_env.sh
+
+# VIM
+rm ~/.vimrc
+ln -s ~/.config-files/.vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp ~/.config-files/vim/colors/jellybeans.vim ~/.vim/colors/.
+vim -c PluginInstall
