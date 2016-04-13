53d77d017bf56dd8977a7f591a1ec42ecf8d2b32
Faylite <benvev95@gmail.com>
2016-04-13 10:14:46 +0200

    Added a script with my common aliases

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+# Navigation
+alias l="ls -l"
+alias ll="ls -la"
+alias c="clear"
+alias ..="cd .."
+
+# Git
+alias g="git"
+alias s="git status"
+alias gl="git log"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+
+# Vim
+alias v="vim"
+alias eb="vim ~/.bashrc"
+alias ev="vim ~/.vimrc"
