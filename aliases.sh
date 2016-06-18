1e4306f9c061961ccc093ff20b1888a5f7958f14
Faylite <benvev95@gmail.com>
2016-06-18 17:58:39 +0200

    Added some aliases

--- aliases.sh
+++ aliases.sh
@@ -3,16 +3,18 @@ alias l="ls -l"
 alias ll="ls -la"
 alias c="clear"
 alias ..="cd .."
+alias :q="exit"
 
 # Git
 alias g="git"
 alias s="git status"
-alias gl="git log"
+alias gl="git log --graph --oneline --decorate --abbrev-commit --all"
 alias ga="git add"
 alias gc="git commit"
 alias gp="git push"
 
 # Vim
 alias v="vim"
+alias vi="vim"
 alias eb="vim ~/.bashrc"
 alias ev="vim ~/.vimrc"
