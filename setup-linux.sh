e863d9aac76446046b430741ce0b834a7c0a5f2b
Faylite <benvev95@gmail.com>
2016-07-07 23:26:28 +0200

    Massive update to setup script

--- setup-linux.sh
+++ setup-linux.sh
@@ -2,14 +2,40 @@
 # Author:faylite
 # Sets up a linux machine to my preferred config
 
-# Install applications
-sudo apt-get install -y zsh vim tmux weechat
+# ------------------------------------------------------- #
+#               Install applications                      #
+# ------------------------------------------------------- #
+$packages = "zsh vim tmux weechat exuberant-ctags curl"
 
-# Install oh-my-zsh
+# ------------------------------------------------------- #
+#                     apt-get                             #
+# ------------------------------------------------------- #
+if [ -z "$(command -v yum)" ]
+then
+	sudo yum check-update
+	sudo yum -y install $packages
+fi
+
+# ------------------------------------------------------- #
+#                     yum                                 #
+# ------------------------------------------------------- #
+if [ -z "$(command -v apt-get)" ]
+then
+	sudo apt-get update
+	sudo apt-get install -y $packages
+fi
+
+# ------------------------------------------------------- #
+#               Install oh-my-zsh                         #
+# ------------------------------------------------------- #
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mkdir ~/.oh-my-zsh/custom/themes
 ln -s ~/.config-files/faylite.zsh-theme ~/.oh-my-zsh/custom/themes/faylite.zsh-theme
 
+
+# ------------------------------------------------------- #
+#                      Shells                             #
+# ------------------------------------------------------- #
 # Create the local_env script if it doesn't exist, sourced by zsh for env variables that shouldn't be in version control
 touch ~/.local_env.sh
 
@@ -21,22 +47,31 @@ echo source ~/.config-files/.bash-aliases.sh >> ~/.bashrc
 rm ~/.zshrc
 ln -s ~/.config-files/.zshrc ~/.zshrc
 
-# VIM
+
+# ------------------------------------------------------- #
+#                      Vim                                #
+# ------------------------------------------------------- #
 rm ~/.vimrc
 ln -s ~/.config-files/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ~/.config-files/vim/colors/jellybeans.vim ~/.vim/colors/.
-vim -c PluginInstall
+# Run vim command and exit
+vim +BundleInstall +qall
 
-# Tmux
+# ------------------------------------------------------- #
+#                      Tmux                               #
+# ------------------------------------------------------- #
 rm ~/.tmux.conf
 ln -s ~/.config-files/.tmux.conf ~/.tmux.conf
 
-# Weechat
+# ------------------------------------------------------- #
+#                      Weechat                            #
+# ------------------------------------------------------- #
 rm ~/.weechat/weechat.conf
 ln -s ~/.config-files/weechat.conf ~/.weechat/weechat.conf
 
-# GNU debugger
+# ------------------------------------------------------- #
+#                   GNU Debugger                          #
+# ------------------------------------------------------- #
 rm ~/.gdbinit
 ln -s ~/.config-files/.gdbinit ~/.gdbinit
-
