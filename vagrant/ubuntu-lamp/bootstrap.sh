350a7412c7d7013b6409aeb49d20481343fe8c6c
Faylite <benvev95@gmail.com>
2016-07-12 04:39:37 +0200

    Updated vagrant scripts

--- vagrant/ubuntu-lamp/bootstrap.sh
+++ vagrant/ubuntu-lamp/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Some vars
-PASSWORD='root'
+PASSWORD='s9pe854yugt9e84gh'
 
 # Set MySQL root password
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
@@ -15,16 +15,37 @@ apt-get install -y apache2
 apt-get install -y php5
 apt-get install -y mysql-server-5.5 php5-mysql
 
-# Link the default www dir to the shared vagrant dir
-if ! [ -L /var/www ]; then
-	rm -rf /var/www
-	ln -fs /vagrant /var/www
-fi
-
 # Configure PHP
 sed -i '/display_errors = Off/c display_errors = on' /etc/php5/apache2/php.ini
 sed -i '/error_reporting = E_ALL & ~E_DEPRECATED/c error_reporting = E_ALL | E_STRICT' /etc/php5/apache2/php.ini
 sed -i '/html_errors = Off/c html_errors = on' /etc/php5/apache2/php.ini
 
+# Setup vhosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+	ServerAdmin webmaster@localhost
+	DocumentRoot /var/www/site/public
+
+	<Directory /var/www/site/public>
+		AllowOverride all
+		Require all granted
+	</Directory>
+
+	ErrorLog \${APACHE_LOG_DIR}/error.log
+	CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOF
+)
+
+echo "$VHOST" > /etc/apache2/sites-available/000-default.conf
+
+# Enable mod_rewrite
+sudo a2enmod rewrite
+
 # Restart apache2
 service apache2 restart
+
+# Run installation script for the project
+(cd /var/www/site/ && php install.php)
