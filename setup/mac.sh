768b9e3c7175c88bd0f21ee91df2c5e5b903dc9b
Benjamin <benvev95@gmail.com>
2021-03-01 13:13:28 +0100

    Update homebrew installation script

--- setup/mac.sh
+++ setup/mac.sh
@@ -7,7 +7,7 @@
 # ------------------------------------------------------- #
 #                  Setup homebrew                         #
 # ------------------------------------------------------- #
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # ------------------------------------------------------- #
 #               Install applications                      #
