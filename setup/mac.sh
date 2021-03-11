00c7702f547f472f12695c7ce118e49856ec4769
Benjamin <benvev95@gmail.com>
2021-03-11 23:57:52 +0100

    Updated configuration with new settings for mac

--- setup/mac.sh
+++ setup/mac.sh
@@ -9,15 +9,24 @@
 # ------------------------------------------------------- #
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+touch ~/.local_env.sh
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.local_env.sh
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # ------------------------------------------------------- #
 #               Install applications                      #
 # ------------------------------------------------------- #
-packages="zsh vim tmux weechat ctags curl wget"
+packages="zsh vim tmux weechat ctags curl wget docker"
 
 if [ ! -z "$(command -v brew)" ]
 then
 	brew update
+
 	brew install $packages
+
+	brew install --cask docker
+	brew install --cask iterm2
 fi
 
 source ~/.config-files/setup/common.sh
